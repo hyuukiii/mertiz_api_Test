e9543f94f88ea4854bfd1eda27d2fc0486e57ecf
dbsgusrl1234567@naver.com <dbsgusrl1234567@naver.com>
2026-03-06 11:03:32 +0900

    1.feat/refacotr : 엑셀 다운로드, 팝업 추가

--- src/main/java/dto/InsuInfoDTO.java
+++ src/main/java/dto/InsuInfoDTO.java
@@ -1,21 +1,27 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor // 기본 생성자 ( 웹스퀘어 JSON 변환 시 필수 )
+@AllArgsConstructor // 모든 필드를 갖춘 생성자( 빌더 작동 시 필수 )
 public class InsuInfoDTO {
 	private String gender;
-	private int age;
+	private Integer age;
 	private String jobClass;
 	private String diseaseCode;
 	private String diseaseHistory;
 	
-	private double height;
-	private double weight;
+	private Double height;
+	private Double weight;
 	private String smokeYn;
 	private String drinkFreq;
 	
 	// 계산 결과값을 담을 필드
-	private int premium;
+	private Integer premium;
 	private String resultMsg;
 }
